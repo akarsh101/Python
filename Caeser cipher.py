4203b732f648cc8e7159dd6acba3bcbedb342049
akarsh101 <46913333+akarsh101@users.noreply.github.com>
2020-12-24 03:09:57 +0530

    Add files via upload

---
+++ Caeser cipher.py
@@ -0,0 +1,30 @@
+output= []
+name = input("enter the elemesnts u want to change\n")
+key= input('Enter Your head alphabet\n')[0]
+number_key= [ord(key) - 96 for key in key]
+number_name = [ord(name) - 96 for name in name]
+sub=[x - number_key[0] for x in number_name]
+if ((name.isupper())and(key.isupper())):
+    for n in number_key:
+        n=n
+    for num in sub:
+        if num >= 0:
+            num=(num+(n*2))
+        else:
+            num=((num*-1)+1)
+        output.append(chr(ord('`')+num))
+    ended=''.join([str(elem) for elem in output])
+elif ((name.islower())and(key.islower())):
+    for n in number_key:
+        n=n
+       
+    for num in sub:
+        if num >= 0:
+            num=((num+(n*2))-1)
+        else:
+            num=((num*-1)+1)
+        output.append(chr(ord('`')+num))
+    ended=''.join([str(elem) for elem in output])
+    print("\t", ended)
+else:
+    print("error in the input or in the key or the name\n please check and try again")
